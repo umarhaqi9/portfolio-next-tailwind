5f7b797b9945b6b4d1e882b221850cd20b2cb5f9
umarhaqi9 <74255063+umarhaqi9@users.noreply.github.com>
2022-09-11 22:41:18 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -365,7 +365,7 @@ export default function Home() {
       {/* Skills Start */}
         <section id='skills' className='pt-36 pb-32 bg-slate-700 dark:bg-slate-300'>
           <div className='container'>
-            <div className='w-full px-4'>
+            <div className='w-full px-4' data-aos="fade-up">
               <div className='mx-auto text-center mb-16'>
                 <h4 className='font-semibold text-lg text-sky-500 mb-2'>Skills</h4>
                 <h2 className='font-bold text-white text-3xl mb-4 sm:text-4xl lg:text-5xl dark:text-slate-900'>Familiar Skills</h2>
@@ -377,7 +377,7 @@ export default function Home() {
               </div>
             </div>
 
-            <div className='w-full px-4'>
+            <div className='w-full px-4' data-aos="fade-up">
               <div className='flex flex-wrap items-center justify-center'>
                 <a href='https://html.com/' className='max-w-[120px] mx-4 py-4 grayscale opacity-60 transition hover:grayscale-0 hover:opacity-100 duration-500 lg:mx-6 xl:mx-8' target="_blank">
                   <Image src={HTMLPic}/>
